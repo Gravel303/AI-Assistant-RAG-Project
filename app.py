56a05e97a639c6d10909ac2d535c9fde17228ab5
Muzummil Khan <muzummil808@gmail.com>
2026-06-01 12:03:21 +0530

    Added Gemini API integration

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
-st.title("AI Study Assistent")
-st.write("My Fisrt AI Project")
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+st.title("AI Study Assistant")
+
+question = st.text_input(
+    "Ask a question:"
+)
+
+if st.button("Generate Answer"):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=question
+    )
+
+    st.write(response.text)
