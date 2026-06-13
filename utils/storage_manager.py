8ab120208ba61dae42a1b7a3d0201e9b2e2d9041
Muzummil Khan <muzummil808@gmail.com>
2026-06-13 10:33:30 +0530

    updated storage_manager.py for deployement

--- utils/storage_manager.py
+++ utils/storage_manager.py
@@ -1,10 +1,15 @@
 import pickle
 import faiss
+import os
 
 def save_chunks(
     chunks,
     filepath="data/chunks.pkl"
 ):
+    os.makedirs(
+        os.path.dirname(filepath),
+        exist_ok=True
+    )
 
     with open(
         filepath,
@@ -33,7 +38,11 @@ def save_index(
     index,
     filepath="data/faiss.index"
 ):
-
+    os.makedirs(
+        os.path.dirname(filepath),
+        exist_ok=True
+    )
+    
     faiss.write_index(
         index,
         filepath
